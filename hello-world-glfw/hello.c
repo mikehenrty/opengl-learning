7d6874142e8c56c8755f6a95de11ac0e89213ec9
Michael Henretty <michael.henretty@gmail.com>
2016-05-14 15:54:14 +0200

    adding a basic animation

--- hello-world-glfw/hello.c
+++ hello-world-glfw/hello.c
@@ -1,7 +1,73 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <GLFW/glfw3.h>
+
+void error_callback(int error, const char* description)
+{
+      fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  }
+}
 
 int main(int argc, char** argv)
 {
-  exit(0);
+  // Initialize glfw.
+  glfwSetErrorCallback(error_callback);
+  if (!glfwInit()) {
+    return EXIT_FAILURE;
+  }
+
+  // Create glfw window.
+  GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+  glfwSetKeyCallback(window, key_callback);
+
+  // Initialize opengl context.
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  // Create a viewport.
+  float ratio;
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  ratio = width / (float) height;
+  glViewport(0, 0, width, height);
+
+  // Loop until window is closed by user.
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glEnd();
+
+    // Swap front and back buffers.
+    glfwSwapBuffers(window);
+
+    // Poll for and process events.
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
 }
