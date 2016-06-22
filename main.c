198327aec798e0d3843cf97ed91268c7a18bef6a
Michael Henretty <michael.henretty@gmail.com>
2016-06-23 00:31:54 +0200

    create many sprites

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define GAME_WIDTH  800
 #define GAME_HEIGHT 600
@@ -8,36 +10,85 @@
 #include "engine.h"
 #include "sprite.h"
 
-static Sprite *sprite;
-static Sprite *sprite2;
-static Sprite *sprite3;
+typedef struct sprite_attribs {
+  int start_x;
+  int start_y;
+  int animation;
+} sprite_attribs;
+
+static Sprite *sprites[MAX_SPRITES];
+static sprite_attribs attribs[MAX_SPRITES * 3];
+static int sprite_count = 0;
 
 static void key_callback(int key)
 {
   // TODO: do something with the keys once we figure out function pointers.
 }
 
+int get_random_number(int max)
+{
+  static int seed = 0;
+  if (seed == 0) {
+    seed = time(NULL);
+    srand((unsigned)seed);
+  }
+  return rand() % max;
+}
+
+Sprite *create_random_sprite()
+{
+  int dimension = get_random_number(100);
+  Sprite *sprite = Sprite_new(dimension, dimension);
+
+  int start_x = get_random_number(GAME_WIDTH);
+  int start_y = get_random_number(GAME_HEIGHT);
+  attribs[sprite_count].start_x = start_x;
+  attribs[sprite_count].start_y = start_y;
+  Sprite_set_position(sprite, start_x, start_y);
+  attribs[sprite_count].animation = get_random_number(2);
+
+  ++sprite_count;
+  return sprite;
+}
+
 
 int init_world()
 {
-  sprite = Sprite_new(100, 100);
-  sprite2 = Sprite_new(100, 100);
-  sprite3 = Sprite_new(100, 100);
-  if (!sprite || !sprite2 || !sprite3) {
-    Log("Sprite creation failed");
-    return 0;
+  for (int i = 0; i < MAX_SPRITES; i++) {
+    sprites[i] = create_random_sprite();
+    if (!sprites[i]) {
+      Log("Sprite creation failed");
+      return 0;
+    }
   }
   return 1;
 }
 
 void update_world() {
   double elapsed = Engine_get_elapsed_time();
-  Sprite_set_position(sprite, 400 + sinf((float)elapsed * 4) * 200.0f,
-                              300 + cosf((float)elapsed * 2) * 200.0f);
-  Sprite_set_position(sprite2, 400 + cosf((float)elapsed * 4) * 200.0f,
-                              300 + sinf((float)elapsed * 2) * 200.0f);
-  Sprite_set_position(sprite3, 100 + cosf((float)elapsed * 4) * 200.0f,
-                              100 + sinf((float)elapsed * 2) * 200.0f);
+  float new_x, new_y;
+
+  for (int i = 0; i < sprite_count; i++) {
+    if (attribs[i].animation == 1) {
+      new_x = attribs[i].start_x + sinf((float)elapsed * 4) * 200.0f;
+      new_y = attribs[i].start_y + cosf((float)elapsed * 2) * 200.0f;
+    } else {
+      new_x = attribs[i].start_x + cosf((float)elapsed * 4) * 200.0f;
+      new_y = attribs[i].start_y + sinf((float)elapsed * 2) * 200.0f;
+    }
+    Sprite_set_position(sprites[i], new_x, new_y);
+  }
+}
+
+int init_engine()
+{
+  if (!Engine_init(GAME_WIDTH, GAME_HEIGHT)) {
+    return 0;
+  }
+  Engine_print_hardware_info();
+  Engine_log_fps();
+  Engine_set_key_callback(key_callback);
+  return 1;
 }
 
 void start_main_loop()
@@ -47,19 +98,17 @@ void start_main_loop()
     Engine_draw_everything();
     Engine_print_gl_error("Rendering");
   }
+
+  // Now that the loop has excited, print any logs.
+  Engine_print_program_log();
 }
 
 int main(int argc, char** argv)
 {
-  int result = Engine_init(GAME_WIDTH, GAME_HEIGHT);
-  if (!result) {
+  if (!init_engine()) {
+    Log("Unable to init engine");
     return -1;
   }
-
-  Log_info("Running app");
-  Engine_print_hardware_info();
-  Engine_log_fps();
-  Engine_set_key_callback(key_callback);
 
   if(!init_world()) {
     Log("Unable to create world, exiting");
@@ -67,6 +116,5 @@ int main(int argc, char** argv)
   }
 
   start_main_loop();
-  Engine_print_program_log();
   return 0;
 }
